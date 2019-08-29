c02ee64d7a97ce1929474abb65e8155d9b4cdf42
vincent <14941597+vincentinttsh@users.noreply.github.com>
2019-08-29 20:41:23 +0800

    add website info

--- Open_class/about/views.py
+++ Open_class/about/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
 def privacy(request):
-    return render(request, 'about/privacy.html' )
+    return render(request, 'about/privacy.html')
 def terms(request):
-    return render(request, 'about/terms.html' )
+    return render(request, 'about/terms.html')
+def about(request):
+    return render(request, 'about/about.html' )
