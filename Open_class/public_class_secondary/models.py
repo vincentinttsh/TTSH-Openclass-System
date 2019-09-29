ec66a1982c54e9e1f0417e7db40596f384f6778b
vincentinttsh <vincentinttsh@gmail.com>
2019-09-29 18:52:33 +0800

    nav change to use bootstrap, and fix some bug

--- Open_class/public_class_secondary/models.py
+++ Open_class/public_class_secondary/models.py
@@ -5,7 +5,7 @@ from account.models import User
 class Attend_data(models.Model) :
     attend_password = models.CharField(max_length = 10, verbose_name='參加密碼')
     attend_number = models.IntegerField(default=0, verbose_name='參加人數')
-    attend_people = models.ManyToManyField(User, verbose_name='參加的人',related_name='attend_class')
+    attend_people = models.ManyToManyField(User, verbose_name='參加的人',related_name='attend_class', blank=True)
     class Meta:
         verbose_name, verbose_name_plural = '國中公開觀課報名人數', '國中公開觀課報名人數'
     def __str__(self):
@@ -24,7 +24,7 @@ class Secondary_Class(models.Model) :
     teach_date = models.DateField(verbose_name='上課日期')
     teach_start_time = models.TimeField(verbose_name='上課開始時間')
     teach_end_time = models.TimeField(verbose_name='上課結束時間')
-    teaching_photo = models.URLField(verbose_name='上課照片', default = '')
+    teaching_photo = models.URLField(verbose_name='上課照片', default = '', blank=True)
     attend_data = models.OneToOneField(Attend_data, on_delete=models.CASCADE, verbose_name='參加資料', related_name='the_class')
     class Meta:
         verbose_name, verbose_name_plural = '國中公開觀課報名資料', '國中公開觀課報名資料'
